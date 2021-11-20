7d6f9e55a68b0dc8011f1ff4c05350f75ede8912
caloriscz <caloris@caloris.cz>
2021-11-20 11:55:16 +0100

    uložená hesla, přihlášení

---
+++ config/env.dist.php
@@ -0,0 +1,9 @@
+<?php
+// nastav si správná data: místo složených závorek napiš správné hodnoty a soubor si přejmenuj na env.php.
+$db = new PDO('mysql:host=localhost;dbname={jméno databáze}', '{uživatel databáze}', '{heslo k databázi}',  array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+
+$name1 == "tobih3107";
+$password1 == "TOKIJ332211";
+
+$name2 == "lukas";
+$password2 == "LUKIJ332211";
