4d376e2e072279618cdda00c50e018e9b64dd2fe
caloriscz <caloris@caloris.cz>
2021-11-20 12:01:34 +0100

    config

--- config/env.dist.php
+++ config/env.dist.php
@@ -1,7 +1,6 @@
 <?php
-// nastav si správná data
-
-$db = new PDO('mysql:host=localhost;dbname=23507_tobikovo', '23507_45005', 'Xmas-2020',  array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+// nastav si správná data. Zaměň hodnoty za uživatele a heslo a název db
+$db = new PDO('mysql:host=localhost;dbname={název db}', '{název uživatele}', '{heslo k db}',  array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 
 $name1 == "{doplň uživatele č. 1 místo složených závorek}";
 $password1 == "{doplň heslo uživatele č. 1 místo složených závorek}";
