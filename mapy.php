cc9af417a1caa07f677c7aaadf37ddb56a6a70fa
caloriscz <caloris@caloris.cz>
2021-11-20 11:42:54 +0100

    přidán soubor mapy.php

---
+++ mapy.php
@@ -0,0 +1,18 @@
+<?php
+include('nav.php');
+echo head("Minecraft mapy");
+?>
+
+<h1 title="mapy">Minecraft mapy </h1>
+
+<h2>Bílá stezka</h2>
+
+<a href="files/parkur_mapa_1.jpg"><img src="files/parkur_mapa_1.jpg" style="width: 200px;"></a>
+<a href="files/parkur_mapa_2.jpg"><img src="files/parkur_mapa_2.jpg" style="width: 200px;"></a>
+<a href="files/parkur_mapa_3.jpg"><img src="files/parkur_mapa_3.jpg" style="width: 200px;"></a>
+<a href="files/parkur_mapa_4.jpg"><img src="files/parkur_mapa_4.jpg" style="width: 200px;"></a>
+<p><a href="files/parkur_map_hotovo.zip">Stáhni si mapu, kterou jsem vytvořil.</a></p>
+
+<h2>Jungle stezka</h2>
+
+<a href="files/map_com.zip">Stáhni</a>
